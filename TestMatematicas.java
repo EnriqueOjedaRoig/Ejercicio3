2f6d21a6ef484ba47b460b8c7aef3dd9952131a7
Enrique <fidel_69_13@hotmail.com>
2015-11-16 14:35:08 +0100

    TestMatematicas modificado en nueva rama

--- TestMatematicas.java
+++ TestMatematicas.java
@@ -9,6 +9,12 @@ public class TestMatematicas {
 		int numero = in.nextInt();
 		boolean par = Matematicas.esPar(numero);
 		System.out.println("Es "+par); 
+		
+		boolean divisible3 = Matematicas.esDivisiblePorTres(numero);
+		System.out.println("Es divisible por 3?: "+divisible3); 
+		
+		boolean divisible5 = Matematicas.esDivisiblePorCinco(numero);
+		System.out.println("Es divisible por 5?: "+divisible5); 
 	}
 }
 
