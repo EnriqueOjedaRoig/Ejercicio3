065783a6212c5f91e1383dd9cd4ce58f007c1ec5
Enrique <fidel_69_13@hotmail.com>
2015-11-16 14:30:05 +0100

    Matematicas modificado en nueva rama

--- Matematicas.java
+++ Matematicas.java
@@ -12,5 +12,26 @@ public class Matematicas{
 		return cierto;
 	}
 
-
+	public static boolean esDivisiblePorTres(int numero){
+	
+		boolean cierto = false;
+		
+		if(numero%3 == 0){
+			cierto = true;
+		}
+		
+		return cierto;
+	}
+	
+	public static boolean esDivisiblePorCinco(int numero){
+	
+		boolean cierto = false;
+		
+		if(numero%5 == 0){
+			cierto = true;
+		}
+		
+		return cierto;
+	}
+	
 }
